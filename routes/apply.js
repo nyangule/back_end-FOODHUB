5edaeb62d3ba0671e3a34a89b791d9826a931f05
Kabza201 <maleteck@gmail.com>
2020-03-19 07:12:37 -0700

    changes

--- routes/apply.js
+++ routes/apply.js
@@ -6,34 +6,34 @@ const datb = require('../database/database');
 
 router.post ('/application',(req,res)=>{
 
-    let rest={
-        restuarant_id:req.body.restuarant_id,
-        //tax_number:req.body.tax_number,
-        restuarant_name:req.body.restuarant_name,
-        address:req.body.address,
-        password:req.body.password,
-        email_address:req.body.email_address
+        let rest={
+          restuarant_id:req.body.restuarant_id,
+          restuarant_name:req.body.restuarant_name,
+          address:req.body.address,
+          password:req.body.password,
+          email_address:req.body.email_address
 
-      }
-     if(!rest)
-     {
-       res.send({'message': 'false'})
-     }
+        }
+        if(!rest)
+          {
+            res.send({'message': 'false'})
+          }
 
       datb.query('SELECT * FROM restuarant_admin  where email_address = ?', rest.email_address, (error, results)=>{
-     if(results[0]){
-      res.send({'message':'restaurant already exits'});
-    }else{
-      datb.query('INSERT INTO restuarant_admin set ?', [rest], (error, results)=>{
-        if(error){
-          res.send({'message':'Something went wrong!'});
+        if(results[0]){
+          res.send({'message':'restaurant already exits'});
         }else{
-          res.send({'message':'Application successfully submitted!'});
+          datb.query('INSERT INTO restuarant_admin set ?', [rest], (error, results)=>{
+            if(error){
+              res.send({'message':'Something went wrong!'});
+            }else{
+              res.send({'message':'Application successfully submitted!'});
+            }
+          })
+        /**/ 
         }
-      })
-    }
-    }) 
-    });
+      }) 
+  });
     
 
 
--- routes/registration.js
+++ routes/registration.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mysql = require('mysql');
 const datb = require('../database/database');
 var bcrypt = require('bcrypt-nodejs');
+const nodemailer = require('nodemailer');
 
 router.post ('/cust_register',(req,res)=>{
 
@@ -13,21 +14,53 @@ router.post ('/cust_register',(req,res)=>{
     email_address:req.body.email_address,
     cell_no:req.body.cell_no,
     password:bcrypt.hashSync(req.body.password, null, null)  
-  }
+  };
 
-  datb.query('SELECT * FROM customer where email_address = ?', cust.email_address, (error, results)=>{
-    if(results[0]){
-      res.send({'message':'user already exist'});
-    }else{
-      datb.query('INSERT INTO customer set ?', [cust], (error, results)=>{
-        if(error){
-          res.send({'message':'Something went wrong!'});
-        }else{
+  if(!cust.name||!cust.surname||!cust.address||!cust.email_address||!cust.cell_no||!cust.password){
+      return res.send({message: 'All fields required...!!'})
+  }else 
+    if(cust.password.length < 10){
+      res.send({ 'message': 'Password must be atleast 9 characters' }); 
+      res.end();
+    return res.send(error);
+  }else{
+    datb.query('SELECT * FROM customer where email_address = ?', [cust.email_address], (error, results)=>{
+      if(results[0]){
+        res.send({'message':'user already exist'});
+      }else{
+        datb.query('INSERT INTO customer set ?', [cust], (error, results)=>{
+          if(error){
+            res.send({'message':'Something went wrong!'});
+          }else{
             res.send({'message':'User successfully Registered!'});
-        }
-      })
-    }
-  })  
+          }
+        })
+        var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'kasifoodhub@gmail.com',
+            pass: '2020#012bc'
+          }
+        });
+        
+        var mailOptions = {
+          from: 'kasifoodhub@gmail.com',
+          to: cust.email_address,
+          subject: 'Ekasi Foodhub',
+          text: 'You are successfully registered please wait to be approved'
+        };
+        
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+            
+          }
+        });
+      }
+    }) 
+  } 
 });
 
 router.post ('/restu_register',(req,res)=>{
