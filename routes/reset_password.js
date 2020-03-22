25103311b2a5097960ce6120052b477e197f29f5
Kabza201 <maleteck@gmail.com>
2020-03-22 13:48:49 -0700

    latest commit

--- routes/reset_password.js
+++ routes/reset_password.js
@@ -1,41 +1,43 @@
-// const express = require('express');
-// const mysql = require('mysql');
-// const router = express.Router();
-// const datb = require('../database/database');
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+const datb = require('../database/database');
 
-
-// router.put('/reset_password', function(req, res) {
+//update customer password
+router.put('/reset_password', function(req, res) {
      
-//     var e_email = req.body.email_address;
-//     var newPassword = req.body.newPassword;
-//     var confirmPassword = req.body.confirmPassword;
+    var e_email = req.body.email_address;
+    var newPassword = req.body.newPassword;
+    var confirmPassword = req.body.confirmPassword;
 
-//     let pass = { 
-//         password: newPassword
-//            }
+    let pass = { 
+        password: newPassword
+           }
    
-//     if(newPassword != confirmPassword){
-//         res.send({"message":"confirm password is not matched"})
-//     }
-//     else
-//     {
-//     datb.query('update customer set ? where email = "'+e_email+'"',[pass],(error,result)=>{
+    if(newPassword != confirmPassword){
+        res.send({"message":"confirm password is not matched"})
+    }
+    else
+    {
+    datb.query('update customer set ? where email_address = "'+e_email+'"',[pass],(error,result)=>{
         
-//         if(error){
-//             res.send({"message":"error occurred"});
-//         }
-//         else{
-//             if(e_email != req.body.email_address){
-//                 res.send({"message":"Email not registered!"});  
-//                 } 
-//         else{
+        if(error){
+            res.send({"message":"error occurred"});
+        }
+        else{
+            if(e_email != req.body.email_address){
+                res.send({"message":"Email not registered!"});  
+                } 
+        else{
             
-//               res.send({"message":"Password has been successfully changed"}); 
+              res.send({"message":"Password has been successfully changed"}); 
             
-//         }
-//         }
-//     });
-//     }
-// });
+        }
+        }
+    });
+    }
+});
 
-// module.exports = router;
+//update vendor password
+
+module.exports = router;
