1bc93f485efd09393e04b4ebae6f29eea7d085c7
Kabza201 <maleteck@gmail.com>
2020-03-22 13:50:10 -0700

    latest commit

--- routes/reset_password.js
+++ routes/reset_password.js
@@ -38,6 +38,39 @@ router.put('/reset_password', function(req, res) {
     }
 });
 
-//update vendor password
 
+//update vendor password
+router.put('/reset_password', function(req, res) {
+     
+    var e_email = req.body.email_address;
+    var newPassword = req.body.newPassword;
+    var confirmPassword = req.body.confirmPassword;
+
+    let pass = { 
+        password: newPassword
+           }
+   
+    if(newPassword != confirmPassword){
+        res.send({"message":"confirm password is not matched"})
+    }
+    else
+    {
+    datb.query('update restuarant_admin set ? where email_address = "'+e_email+'"',[pass],(error,result)=>{
+        
+        if(error){
+            res.send({"message":"error occurred"});
+        }
+        else{
+            if(e_email != req.body.email_address){
+                res.send({"message":"Email not registered!"});  
+                } 
+        else{
+            
+              res.send({"message":"Password has been successfully changed"}); 
+            
+        }
+        }
+    });
+    }
+});
 module.exports = router;
