af701b9ae0a6c45435e1d583a07f85067d1bfc8d
Kabza201 <maleteck@gmail.com>
2020-03-22 13:45:53 -0700

    latest commit

--- routes/forgot.js
+++ routes/forgot.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const router = express.Router();
 const datb = require('../database/database');
 
-/* GET users listing. */
+//customer forgot their password
 router.get('/cust_forgot_pass', function(req, res) {
   
     var email = req.body.email_address;
@@ -22,4 +22,23 @@ router.get('/cust_forgot_pass', function(req, res) {
 
 });
 
+
+//vendor forgot their password
+router.get('/vendor_forgot_pass', function(req, res) {
+  
+    var email = req.body.email_address;
+
+    datb.query('select * from restuarant_admin where email_address = ?',[email],(error,result)=>{
+        if(error){
+            res.send({"message":"error ocurred"})
+        } else{
+            if(result[0]){
+                res.send({"message":"Reset password link sent to the registered email"});       
+            } else{
+                res.send({"message":"This email is not registered "});
+            }
+        }
+    });
+
+});
 module.exports = router;
--- routes/reset_password.js
+++ routes/reset_password.js
@@ -2,3 +2,40 @@
 // const mysql = require('mysql');
 // const router = express.Router();
 // const datb = require('../database/database');
+
+
+// router.put('/reset_password', function(req, res) {
+     
+//     var e_email = req.body.email_address;
+//     var newPassword = req.body.newPassword;
+//     var confirmPassword = req.body.confirmPassword;
+
+//     let pass = { 
+//         password: newPassword
+//            }
+   
+//     if(newPassword != confirmPassword){
+//         res.send({"message":"confirm password is not matched"})
+//     }
+//     else
+//     {
+//     datb.query('update customer set ? where email = "'+e_email+'"',[pass],(error,result)=>{
+        
+//         if(error){
+//             res.send({"message":"error occurred"});
+//         }
+//         else{
+//             if(e_email != req.body.email_address){
+//                 res.send({"message":"Email not registered!"});  
+//                 } 
+//         else{
+            
+//               res.send({"message":"Password has been successfully changed"}); 
+            
+//         }
+//         }
+//     });
+//     }
+// });
+
+// module.exports = router;
